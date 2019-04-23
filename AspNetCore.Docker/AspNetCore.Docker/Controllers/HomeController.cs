f3b5121fa1932bc7f9d3e918136dedb61b5452ed
wuhb <156690381@qq.com>
2019-04-23 09:13:50 +0800

    hengbin,2019-04-23

---
+++ AspNetCore.Docker/AspNetCore.Docker/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Docker.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.msg = "abc";// Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            //ViewBag.Content = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            return View();
+        }
+    }
+}
---
+++ AspNetCore.Docker/AspNetCore.Docker/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore.Docker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Msg { get; set; }
+
+        public void OnGet()
+        {
+            Msg = Environment.GetEnvironmentVariable("X-Path", EnvironmentVariableTarget.Machine);//"abc";
+            if (string.IsNullOrEmpty(Msg))
+                Msg = "default";
+        }
+    }
+}
